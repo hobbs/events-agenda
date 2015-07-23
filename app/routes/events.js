6efe2f0e3b6ea369786ae3ba79739b7b5e5d9671
Zach Hobbs <zhobbs@wikia-inc.com>
2015-07-23 15:36:34 -0700

    Added routes for all/featured/starred

--- app/routes/events.js
+++ app/routes/events.js
@@ -1,16 +1,5 @@
 export default Ember.Route.extend({
   model: function () {
 	  return this.store.findAll('event');
-  },
-	actions: {
-		setEventsModel: function (modelName) {
-			this.set('model', modelName);
-		}
-	},
-	events: function () {
-		return this.store.findAll('event');
-	},
-	headlineEvents: function () {
-		return this.store.findAll('headlineEvent');
-	}
+  }
 });
