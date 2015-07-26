96a779e6bc0f84f8aa44721fec2a24100403c7ac
Zach Hobbs <zach@tunewiki.com>
2015-07-26 13:27:24 -0700

    Fixed a few bugs on mobile safari

--- app/app.js
+++ app/app.js
@@ -10,7 +10,10 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+
+  customEvents: {touchend: "click"}
+  
 });
 
 loadInitializers(App, config.modulePrefix);
